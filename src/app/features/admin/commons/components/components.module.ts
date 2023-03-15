db4c898323a329c1d6564af1402b4cd8171200fe
IsaacCRS17 <crs170919@gmail.com>
2023-03-14 19:56:41 -0500

    Revert "Revert "Revert "Revert "Merge branch 'UpdateIR'""""
    This reverts commit ced5107e92210be842f5777cf6641b95975aa67a.

--- src/app/features/admin/commons/components/components.module.ts
+++ src/app/features/admin/commons/components/components.module.ts
@@ -18,6 +18,8 @@ import { TableGradePeriodComponent } from './tables-data/table-grade-period/tabl
 import { TableNewsComponent } from './tables-data/table-news/table-news.component';
 import { TableImageComponent } from './tables-data/table-image/table-image.component';
 import { TableAnioLectivoComponent } from './tables-data/table-anio-lectivo/table-anio-lectivo.component';
+import { TableRoleComponent } from './tables-data/table-role/table-role.component';
+
 
 
 
@@ -39,7 +41,7 @@ import { TableAnioLectivoComponent } from './tables-data/table-anio-lectivo/tabl
     TableNewsComponent,
     TableImageComponent,
     TableAnioLectivoComponent
-    
+    TableRoleComponent
   ],
   exports:[
     TableComponent,
@@ -57,6 +59,7 @@ import { TableAnioLectivoComponent } from './tables-data/table-anio-lectivo/tabl
     TableNewsComponent,
     TableImageComponent,
     TableAnioLectivoComponent
+    TableRoleComponent
   ],
   imports: [
     CommonModule,
