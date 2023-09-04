ae503d28c567a0e71193d913cea029ec4254d424
IsaacCRS17 <crs170919@gmail.com>
2023-09-03 19:06:07 -0500

    "updates"

--- src/app/features/admin/interfaces/user.ts
+++ src/app/features/admin/interfaces/user.ts
@@ -3,7 +3,7 @@ import { ITeacher } from "./teacher";
 export interface IUser{
     id:string;
     code: string;
-    nombreUsuario: string;
+    nombreUsuario?: string;
     name: string;
     pa_surname: string;
     ma_surname: string;
@@ -13,7 +13,7 @@ export interface IUser{
     tel: string;
     gra_inst: string;
     email: string;
-    password:string;
-    rol:string; 
-    
+    password?:string;
+    rol:string;
+
 }
